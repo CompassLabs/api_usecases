4b930e340fb0e6278087cdff828973879966bf12
royalnine <17271567+royalnine@users.noreply.github.com>
2025-06-03 19:02:58 +0500

    before deep debugging

--- smart_accounts_safe/src/index.ts
+++ smart_accounts_safe/src/index.ts
@@ -1,25 +1,39 @@
 import { createSafeClient } from '@safe-global/sdk-starter-kit';
 import dotenv from 'dotenv';
 import { CompassApiSDK } from '@compass-labs/api-sdk';
+import { SafeProvider } from '@safe-global/protocol-kit';
 
 dotenv.config();
 
-const RPC_URL = process.env.RPC_URL as string;
+const ARBITRUM_RPC_URL = process.env.ARBITRUM_RPC_URL as SafeProvider['provider'];
 const SIGNER_PRIVATE_KEY = process.env.SIGNER_PRIVATE_KEY as string;
 const SIGNER_ADDRESS = process.env.SIGNER_ADDRESS as string;
 const COMPASS_API_KEY = process.env.COMPASS_API_KEY as string;
+const SAFE_ADDRESS = process.env.SAFE_ADDRESS as string;
 
 const main = async () => {
-    const safeClient = await createSafeClient({
-        provider: RPC_URL,
+    // NOTE: if creating a new safe client, we need to use this
+    let safeClient;
+
+    // if (SAFE_ADDRESS === undefined) {
+    safeClient = await createSafeClient({
+        provider: ARBITRUM_RPC_URL,
         signer: SIGNER_PRIVATE_KEY,
         safeOptions: {
             owners: [SIGNER_ADDRESS],
             threshold: 1,
         },
     });
-
+    // } else {
+    // // Else, we can use the existing safe client
+    //     safeClient = await createSafeClient({
+    //         provider: ARBITRUM_RPC_URL,
+    //         signer: SIGNER_PRIVATE_KEY,
+    //         safeAddress: SAFE_ADDRESS,
+    //     });
+    // }
     console.log('Safe client created at:', await safeClient.getAddress());
+    console.log('is Safe client deployed:', await safeClient.isDeployed());
 
     const compassApiSDK = new CompassApiSDK({
         apiKeyAuth: COMPASS_API_KEY,
