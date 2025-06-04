4e78a5f47c54f3d034106ec166e7dc55a74ee61c
royalnine <17271567+royalnine@users.noreply.github.com>
2025-06-04 16:07:35 +0500

    formatting

--- smart_accounts_safe/src/index.ts
+++ smart_accounts_safe/src/index.ts
@@ -13,81 +13,80 @@ const COMPASS_API_KEY = process.env.COMPASS_API_KEY as string;
 const SAFE_ADDRESS = process.env.SAFE_ADDRESS as string;
 
 const main = async () => {
+  // NOTE: if we need to deploy a new safe, uncomment the following code
+  // const safeClient = await createSafeClient({
+  //     provider: ARBITRUM_RPC_URL,
+  //     signer: SIGNER_PRIVATE_KEY,
+  //     safeOptions: {
+  //         owners: [SIGNER_ADDRESS],
+  //         threshold: 1,
+  //     },
+  // });
+  // ELSE: use existing safe client:
+  const safeClient = await createSafeClient({
+    provider: ARBITRUM_RPC_URL,
+    signer: SIGNER_PRIVATE_KEY,
+    safeAddress: SAFE_ADDRESS,
+  });
 
-    // NOTE: if we need to deploy a new safe, uncomment the following code
-    // const safeClient = await createSafeClient({
-    //     provider: ARBITRUM_RPC_URL,
-    //     signer: SIGNER_PRIVATE_KEY,
-    //     safeOptions: {
-    //         owners: [SIGNER_ADDRESS],
-    //         threshold: 1,
-    //     },
-    // });
-    // ELSE: use existing safe client:
-    const safeClient = await createSafeClient({
-        provider: ARBITRUM_RPC_URL,
-        signer: SIGNER_PRIVATE_KEY,
-        safeAddress: SAFE_ADDRESS,
-    });
+  console.log('Safe client created at:', await safeClient.getAddress());
 
-    console.log('Safe client created at:', await safeClient.getAddress());
+  const compassApiSDK = new CompassApiSDK({
+    apiKeyAuth: COMPASS_API_KEY,
+  });
 
-    const compassApiSDK = new CompassApiSDK({
-        apiKeyAuth: COMPASS_API_KEY,
-    });
+  const result = await compassApiSDK.smartAccount.accountBatchedUserOperations({
+    chain: 'arbitrum:mainnet',
+    operations: [
+      {
+        body: {
+          actionType: 'ALLOWANCE_INCREASE',
+          token: 'USDC',
+          contractName: 'AaveV3Pool',
+          amount: '3',
+        },
+      },
+      // NOTE: if creating new safe, comment the `AAVE_SUPPLY` operation out
+      // the safe smart account will not have any USDC as it gets
+      // deployed only with the first transaction
+      // so the first transaction will be the allowance increase upon creation
+      // when reusing an existing contract, fund it with 10 USDC and uncomment this operation
+      {
+        body: {
+          actionType: 'AAVE_SUPPLY',
+          token: 'USDC',
+          amount: '3',
+        },
+      },
+    ],
+  });
 
-    const result = await compassApiSDK.smartAccount.accountBatchedUserOperations({
-        chain: 'arbitrum:mainnet',
-        operations: [
-            {
-                body: {
-                    actionType: 'ALLOWANCE_INCREASE',
-                    token: 'USDC',
-                    contractName: 'AaveV3Pool',
-                    amount: '3',
-                },
-            },
-            // NOTE: if creating new safe, comment the `AAVE_SUPPLY` operation out
-            // the safe smart account will not have any USDC as it gets
-            // deployed only with the first transaction
-            // so the first transaction will be the allowance increase upon creation
-            // when reusing an existing contract, fund it with 10 USDC and uncomment this operation
-            {
-                body: {
-                    actionType: 'AAVE_SUPPLY',
-                    token: 'USDC',
-                    amount: '3',
-                },
-            },
-        ],
-    });
+  const operations = result.operations.map((op) => ({
+    to: op.to as `0x${string}`,
+    data: op.data as `0x${string}`,
+    value: op.value ? String(op.value) : '0',
+  }));
 
-    const operations = result.operations.map((op) => ({
-        to: op.to as `0x${string}`,
-        data: op.data as `0x${string}`,
-        value: op.value ? String(op.value) : '0',
-    }));
+  const txResult = await safeClient.send({ transactions: operations });
 
-    const txResult = await safeClient.send({ transactions: operations });
+  // NOTE: if there is more than one signer, we need to create a new safe client for each signer
+  // const safeTxHash = txResult.transactions?.safeTxHash
+  // const newSafeClient = await createSafeClient({
+  //     provider: RPC_URL,
+  //     signer: OTHER_SIGNER_PRIVATE_KEY,
+  //     safeAddress: safeTxHash,
+  // });
+  // const pendingTransactions = await newSafeClient.getPendingTransactions()
+  // for (const transaction of pendingTransactions.results) {
+  //     if (transaction.safeTxHash !== safeTxHash) {
+  //       return
+  //     }
 
-    // NOTE: if there is more than one signer, we need to create a new safe client for each signer
-    // const safeTxHash = txResult.transactions?.safeTxHash
-    // const newSafeClient = await createSafeClient({
-    //     provider: RPC_URL,
-    //     signer: OTHER_SIGNER_PRIVATE_KEY,
-    //     safeAddress: safeTxHash,
-    // });
-    // const pendingTransactions = await newSafeClient.getPendingTransactions()
-    // for (const transaction of pendingTransactions.results) {
-    //     if (transaction.safeTxHash !== safeTxHash) {
-    //       return
-    //     }
+  // const txResult2 = await newSafeClient.confirm({safeTxHash});
 
-    // const txResult2 = await newSafeClient.confirm({safeTxHash});
-
-    console.log('txResult', txResult);
-    console.log('txResult.status', txResult.status);
-    console.log(txResult.transactions?.ethereumTxHash);
+  console.log('txResult', txResult);
+  console.log('txResult.status', txResult.status);
+  console.log(txResult.transactions?.ethereumTxHash);
 };
 
 main();
--- smart_accounts_zerodev/src/index.ts
+++ smart_accounts_zerodev/src/index.ts
@@ -3,9 +3,9 @@ import { privateKeyToAccount } from 'viem/accounts';
 import { arbitrum } from 'viem/chains';
 import { signerToEcdsaValidator } from '@zerodev/ecdsa-validator';
 import {
-	createKernelAccount,
-	createKernelAccountClient,
-	createZeroDevPaymasterClient,
+  createKernelAccount,
+  createKernelAccountClient,
+  createZeroDevPaymasterClient,
 } from '@zerodev/sdk';
 import { getEntryPoint, KERNEL_V3_1 } from '@zerodev/sdk/constants';
 import { CompassApiSDK } from '@compass-labs/api-sdk';
@@ -18,94 +18,94 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY as `0x${string}`;
 const COMPASS_API_KEY = process.env.COMPASS_API_KEY;
 
 if (!ZERODEV_RPC) {
-	throw new Error('ZERODEV_RPC is not set');
+  throw new Error('ZERODEV_RPC is not set');
 }
 
 const chain = arbitrum;
 const publicClient = createPublicClient({
-	transport: http(ZERODEV_RPC),
-	chain,
+  transport: http(ZERODEV_RPC),
+  chain,
 });
 const entryPoint = getEntryPoint('0.7');
 
 const main = async () => {
-	const signer = privateKeyToAccount(PRIVATE_KEY);
+  const signer = privateKeyToAccount(PRIVATE_KEY);
 
-	console.log('My account:', signer);
+  console.log('My account:', signer);
 
-	const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
-		signer,
-		entryPoint,
-		kernelVersion: KERNEL_V3_1,
-	});
+  const ecdsaValidator = await signerToEcdsaValidator(publicClient, {
+    signer,
+    entryPoint,
+    kernelVersion: KERNEL_V3_1,
+  });
 
-	const account = await createKernelAccount(publicClient, {
-		entryPoint,
-		plugins: { sudo: ecdsaValidator },
-		kernelVersion: KERNEL_V3_1,
-	});
+  const account = await createKernelAccount(publicClient, {
+    entryPoint,
+    plugins: { sudo: ecdsaValidator },
+    kernelVersion: KERNEL_V3_1,
+  });
 
-	const zerodevPaymaster = createZeroDevPaymasterClient({
-		chain,
-		transport: http(ZERODEV_RPC),
-	});
+  const zerodevPaymaster = createZeroDevPaymasterClient({
+    chain,
+    transport: http(ZERODEV_RPC),
+  });
 
-	const kernelClient = createKernelAccountClient({
-		account,
-		chain,
-		bundlerTransport: http(ZERODEV_RPC),
-		paymaster: {
-			getPaymasterData(userOperation) {
-				return zerodevPaymaster.sponsorUserOperation({ userOperation });
-			},
-		},
-	});
+  const kernelClient = createKernelAccountClient({
+    account,
+    chain,
+    bundlerTransport: http(ZERODEV_RPC),
+    paymaster: {
+      getPaymasterData(userOperation) {
+        return zerodevPaymaster.sponsorUserOperation({ userOperation });
+      },
+    },
+  });
 
-	const accountAddress = kernelClient.account.address;
-	console.log('My account:', accountAddress);
+  const accountAddress = kernelClient.account.address;
+  console.log('My account:', accountAddress);
 
-	const compassApiSDK = new CompassApiSDK({
-		apiKeyAuth: COMPASS_API_KEY,
-	});
+  const compassApiSDK = new CompassApiSDK({
+    apiKeyAuth: COMPASS_API_KEY,
+  });
 
-	const result = await compassApiSDK.smartAccount.accountBatchedUserOperations({
-		chain: 'arbitrum:mainnet',
-		operations: [
-			{
-				body: {
-					actionType: 'ALLOWANCE_INCREASE',
-					token: 'USDC',
-					contractName: 'AaveV3Pool',
-					amount: '10',
-				},
-			},
-			{
-				body: {
-					actionType: 'AAVE_SUPPLY',
-					token: 'USDC',
-					amount: '10',
-				},
-			},
-		],
-	});
+  const result = await compassApiSDK.smartAccount.accountBatchedUserOperations({
+    chain: 'arbitrum:mainnet',
+    operations: [
+      {
+        body: {
+          actionType: 'ALLOWANCE_INCREASE',
+          token: 'USDC',
+          contractName: 'AaveV3Pool',
+          amount: '10',
+        },
+      },
+      {
+        body: {
+          actionType: 'AAVE_SUPPLY',
+          token: 'USDC',
+          amount: '10',
+        },
+      },
+    ],
+  });
 
-	const operations = result.operations.map((op) => ({
-		to: op.to as `0x${string}`,
-		data: op.data as `0x${string}`,
-		value: op.value ? BigInt(op.value) : undefined,
-	})) as Call[];
+  const operations = result.operations.map((op) => ({
+    to: op.to as `0x${string}`,
+    data: op.data as `0x${string}`,
+    value: op.value ? BigInt(op.value) : undefined,
+  })) as Call[];
 
-	const operationHash = await kernelClient.sendUserOperation({
-		callData: await kernelClient.account.encodeCalls(operations),
-	});
-	console.log('Submitted batched transaction:', operationHash);
+  const operationHash = await kernelClient.sendUserOperation({
+    callData: await kernelClient.account.encodeCalls(operations),
+  });
+  console.log('Submitted batched transaction:', operationHash);
 
-	const operationReceipt = await kernelClient.waitForUserOperationReceipt({
-		hash: operationHash,
-	});
-	console.log('Batched transaction confirmed:', operationReceipt.receipt.transactionHash);
+  const operationReceipt = await kernelClient.waitForUserOperationReceipt({
+    hash: operationHash,
+  });
+  console.log('Batched transaction confirmed:', operationReceipt.receipt.transactionHash);
 
-	process.exit(0);
+  process.exit(0);
 };
 
 main();
