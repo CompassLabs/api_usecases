ff711b0919878e5f7efcb4e872aadfed40c1b479
royalnine <17271567+royalnine@users.noreply.github.com>
2025-06-09 12:19:51 +0500

    fixes

--- aave_looping/python_example/src/main.py
+++ aave_looping/python_example/src/main.py
@@ -1,4 +1,4 @@
-from compass_api_sdk import CompassApiSDK
+from compass_api_sdk import CompassAPI
 import os
 import dotenv
 from web3 import Web3
@@ -11,7 +11,7 @@ RPC_URL = os.getenv("RPC_URL")
 COMPASS_API_KEY = os.getenv("COMPASS_API_KEY")
 
 def main():
-    sdk = CompassApiSDK(api_key=COMPASS_API_KEY)
+    sdk = CompassAPI(api_key_auth=COMPASS_API_KEY)
     
     private_key = PRIVATE_KEY
     if not private_key.startswith("0x"):
@@ -25,7 +25,7 @@ def main():
         sender=account.address
     )
     
-    signed_auth = Account.sign_authorization(auth, private_key)
+    signed_auth = Account.sign_authorization(auth.model_dump(by_alias=True), private_key)
     
     looping_tx = sdk.transaction_batching.aave_loop(
         chain="ethereum:mainnet",
@@ -34,13 +34,13 @@ def main():
         collateral_token="USDC",
         borrow_token="WETH",
         initial_collateral_amount=10,
-        multiplier=2.0,
-        max_slippage_percent=1,
-        loan_to_value=80
+        multiplier=1.5,
+        max_slippage_percent=2.5,
+        loan_to_value=70
     )
     
-    signed_tx = w3.eth.account.sign_transaction(looping_tx, private_key)
-    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    signed_tx = w3.eth.account.sign_transaction(looping_tx.model_dump(by_alias=True), private_key)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
     
     print(f"Transaction hash: {tx_hash.hex()}")
 
