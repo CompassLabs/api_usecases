580a897015e94af5b823ecd7ca181c6dda96fdbc
ccosnett <conor142857@icloud.com>
2025-06-11 11:44:58 +0100

    tidy Makefile

--- api_reference/python_sdk_snippets/aave/rate.py
+++ api_reference/python_sdk_snippets/aave/rate.py
@@ -1,8 +1,9 @@
 from compass_api_sdk import CompassAPI, models
 
+from dotenv import
 
 with CompassAPI(
-    api_key_auth="<YOUR_API_KEY_HERE>",
+    api_key_auth=os.env.
 ) as compass_api:
     res = compass_api.aave_v3.rate(
         chain=models.AaveRateChain.ARBITRUM_MAINNET, token=models.AaveRateToken.USDC
