5bbb56e09a2573863e46dc9cee2dc639277bf61a
ccosnett <conor142857@icloud.com>
2025-06-17 15:48:30 +0100

    WIP

--- vault_rebalance/main.py
+++ vault_rebalance/main.py
@@ -2,12 +2,15 @@ import os
 import time
 from decimal import Decimal
 
+
 import streamlit as st
 from dotenv import load_dotenv
 from web3 import Web3, HTTPProvider
 from web3.types import RPCEndpoint
+from eth_account import Account
 
-from compass_api_sdk import CompassAPI
+
+from compass_api_sdk import CompassAPI, models
 from compass_api_sdk.models import (
     Chain,
     TokenEnum,
@@ -20,11 +23,18 @@ from compass_api_sdk.models import (
 
 load_dotenv()
 
+#dotenv.load_dotenv()
+PRIVATE_KEY = os.environ.get("PRIVATE_KEY")
+API_KEY = os.environ.get("COMPASS_KEY")
+
+#print(PRIVATE_KEY)
+
+
 # Setup Compass API and Web3
 compass = CompassAPI(
-    api_key_auth=os.environ.get("COMPASS_KEY"),
-    server_url='http://0.0.0.0:80',
-)
+    api_key_auth=API_KEY
+                     #,server_url='http://0.0.0.0:80'
+    )
 
 WALLET = "0xa829B388A3DF7f581cE957a95edbe419dd146d1B"
 w3 = Web3(HTTPProvider("http://127.0.0.1:8545"))
@@ -37,6 +47,22 @@ usdc_vaults = [
     "0x64964E162Aa18d32f91eA5B24a09529f811AEB8e",  # Re7, USDC Prime
 ]
 
+
+# First get the authorization
+account = Account.from_key(PRIVATE_KEY)
+
+auth = compass.transaction_batching.authorization(
+    chain=models.Chain.ETHEREUM_MAINNET, sender=account.address
+)
+
+auth_dict = auth.model_dump(mode="json", by_alias=True)
+
+# Sign the authorization
+signed_auth = Account.sign_authorization(auth_dict, PRIVATE_KEY)
+
+chain = models.Chain.ETHEREUM_MAINNET
+sender = account.address
+signed_authorization = signed_auth.model_dump(by_alias=True)
 
 # SNIPPET 1 START
 def withdraw_tx(vault: str) -> dict:
@@ -81,6 +107,18 @@ def get_balance() -> Decimal:
     )
     return Decimal(res.amount)
 # SNIPPET 1 END
+
+
+def get_vault_position(vault: str) -> Decimal:
+    res = compass.morpho.vault_position(
+        chain=models.MorphoVaultPositionChain.ETHEREUM_MAINNET,
+        user_address="0xa829B388A3DF7f581cE957a95edbe419dd146d1B",
+        vault_address=vault,
+        server_url='http://0.0.0.0:80'
+    )
+    # Handle response
+    #print(f"vault contains: {res.token_amount} USDC")
+    return Decimal(res.token_amount)
 
 # Streamlit Setup
 st.set_page_config(layout="wide")
@@ -132,28 +170,39 @@ def submit(user_vaults, address2vault, user_positions, target_percentages: list[
     }
     </style>''', unsafe_allow_html=True)
 
-    cols = st.columns(3)
+    bundle = []
+    cols = st.columns(4)
 
     with cols[0]:
         st.markdown("# Withdrawing")
         for vault, position in zip(user_vaults, user_positions):
             with st.expander(label=f"Withdrawing from vault {address2vault[vault].symbol}"):
-                tx = withdraw_tx(vault)
-                st.json(tx)
-            with st.spinner("Sign and submit"):
-                w3.eth.send_transaction(tx)
-                time.sleep(4)
+                bundle.append(
+                    models.UserOperation(body=models.MorphoWithdrawParams(vault_address=vault, amount="ALL"))
+                )
+                st.markdown(bundle)
+            with st.spinner("Adding transaction to Bundle"):
+                #w3.eth.send_transaction(tx)
+                time.sleep(0.5)
 
     with cols[1]:
         st.markdown("# Allowances")
         wallet_quantity = get_balance()
         for vault in user_vaults:
             with st.expander(label=f"Setting new allowance on vault {address2vault[vault].symbol}"):
-                tx = set_allowance_tx(vault, wallet_quantity)
-                st.json(tx)
-            with st.spinner("Sign and submit"):
-                w3.eth.send_transaction(tx)
-                time.sleep(4)
+                bundle.append(
+                    models.UserOperation(
+                        body=models.MorphoSetVaultAllowanceParams(
+                            vault_address=vault, amount=1000
+                        )
+                    )
+                )
+                st.markdown(bundle)
+                #tx = set_allowance_tx(vault, wallet_quantity)
+                #st.json(tx)
+            with st.spinner("Adding transaction to Bundle"):
+                #w3.eth.send_transaction(tx)
+                time.sleep(0.5)
 
     with cols[2]:
         st.markdown("# Re-depositing")
@@ -161,14 +210,55 @@ def submit(user_vaults, address2vault, user_positions, target_percentages: list[
         #     time.sleep(10)
 
         wallet_quantity = get_balance()
+        sum_of_positions = get_vault_position(usdc_vaults[0])+ get_vault_position(usdc_vaults[1])+ get_vault_position(usdc_vaults[2])
+        TOTAL = wallet_quantity + sum_of_positions
+
         for vault, target_percentage in zip(user_vaults, target_percentages):
-            target = wallet_quantity * Decimal(target_percentage) / Decimal(100)
+            target = TOTAL * Decimal(target_percentage) / Decimal(100)
+           # target = float(target) #amount should be a float
             with st.expander(label=f"Supplying {target} to {address2vault[vault].symbol}"):
-                tx = deposit_tx(vault, target)
-                st.json(tx)
-            with st.spinner("Sign and submit"):
-                w3.eth.send_transaction(tx)
-                time.sleep(4)
+                bundle.append(
+                    models.UserOperation(
+                        body=models.MorphoDepositParams(
+                            vault_address=vault, amount=target
+                        )
+                    )
+                )
+                st.markdown(bundle)
+                # tx = deposit_tx(vault, target)
+                # st.json(tx)
+            with st.spinner("Adding transaction to Bundle"):
+                #w3.eth.send_transaction(tx)
+                time.sleep(0.5)
+    with cols[3]:
+        st.markdown("running multicall transaction")
+
+
+
+
+
+
+        res = compass.transaction_batching.execute(
+            chain=chain,
+            sender=sender,
+            signed_authorization=signed_authorization,
+            actions=bundle,
+            server_url="http://0.0.0.0:80",
+        )
+        unsigned_transaction = res.model_dump(by_alias=True)
+
+        st.markdown("SIGNING MULTICALL TRANSACTION")
+        signed_transaction = w3.eth.account.sign_transaction(
+            unsigned_transaction, PRIVATE_KEY
+        )
+        time.sleep(1)
+        st.markdown("BROADCASTING MULTICALL TRANSACTION")
+        time.sleep(1)
+
+        txn_hash = w3.eth.send_raw_transaction(signed_transaction.raw_transaction)
+        st.markdown(txn_hash.hex())
+        time.sleep(1)
+        st.markdown(f"{get_balance()} USDC")
 
     st.rerun()
 
@@ -204,7 +294,7 @@ with cols[1]:
         value = c.slider(
             key=f"slider_{vault.symbol}",
             label="Choose percentage",
-            value=20,
+            value=33,
         )
         sliders.append(value)
 
@@ -227,6 +317,56 @@ with cols[2]:
 
     st.subheader("Compass SDK code")
     t=open('./main.py','r').read()
-    code=t.split('SNIPPET 1 START')[1].split('SNIPPET 1 END')[0]
+    code="""
+    res = compass.transaction_batching.execute(
+    chain=chain,
+    sender=sender,
+    signed_authorization=signed_authorization,
+    actions=[
+        # Set Allowance
+        models.UserOperation(
+            body=models.MorphoWithdrawParams(vault_address=usdc_vaults[0], amount="ALL")
+        ),
+        models.UserOperation(
+            body=models.MorphoWithdrawParams(vault_address=usdc_vaults[1], amount="ALL")
+        ),
+        models.UserOperation(
+            body=models.MorphoWithdrawParams(vault_address=usdc_vaults[2], amount="ALL")
+        ),
+        models.UserOperation(
+            body=models.MorphoSetVaultAllowanceParams(
+                vault_address=usdc_vaults[0], amount=1000
+            )
+        ),
+        models.UserOperation(
+            body=models.MorphoSetVaultAllowanceParams(
+                vault_address=usdc_vaults[1], amount=1000
+            )
+        ),
+        models.UserOperation(
+            body=models.MorphoSetVaultAllowanceParams(
+                vault_address=usdc_vaults[2], amount=1000
+            )
+        ),
+        models.UserOperation(
+            body=models.MorphoDepositParams(
+                vault_address=usdc_vaults[0], amount=2.817543
+            )
+        ),
+        models.UserOperation(
+            body=models.MorphoDepositParams(
+                vault_address=usdc_vaults[1], amount=2.817543
+            )
+        ),
+        models.UserOperation(
+            body=models.MorphoDepositParams(
+                vault_address=usdc_vaults[2], amount=2.902924
+            )
+        ),
+    ],
+    server_url="http://0.0.0.0:80",
+)
+    """
+    # t.split('SNIPPET 1 START')[1].split('SNIPPET 1 END')[0])
     st.code(code)
 
