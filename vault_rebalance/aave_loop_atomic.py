c5e1b82e975ad2f5a506f8dd4ea46695555335e0
ccosnett <conor142857@icloud.com>
2025-06-23 12:41:09 +0100

    add: actions to atomic loop

--- vault_rebalance/aave_loop_atomic.py
+++ vault_rebalance/aave_loop_atomic.py
@@ -1,10 +1,12 @@
-from time import sleep
-
+import time
 from dotenv import load_dotenv
 from web3 import HTTPProvider, Web3
 from web3.types import RPCEndpoint
 import os
+from compass_api_sdk import CompassAPI, models
+from decimal import Decimal
 
+time.sleep(5)
 load_dotenv()
 PRIVATE_KEY = os.getenv("PRIVATE_KEY")
 COMPASS_API_KEY = os.getenv("COMPASS_API_KEY")
@@ -22,15 +24,11 @@ w3.provider.make_request(
     [WALLET, "0x56BC75E2D63100000"],  # Equivalent to 100 ETH in wei
 )
 
-
-
-from compass_api_sdk import CompassAPI, models
-
-
 compass = CompassAPI(api_key_auth=COMPASS_API_KEY)
+chain = models.TokenBalanceChain.ETHEREUM_MAINNET
 
 
-def print_balance() -> float:
+def print_usdc_balance():
     res = compass.token.balance(
             chain=models.TokenBalanceChain.ETHEREUM_MAINNET,
             user=WALLET,
@@ -40,7 +38,17 @@ def print_balance() -> float:
     print(f" USDC balance: {res.amount}")
     pass
 
-def print_eth_balance() -> float:
+def print_weth_balance():
+    res = compass.token.balance(
+            chain=models.TokenBalanceChain.ETHEREUM_MAINNET,
+            user=WALLET,
+            token=models.TokenEnum.WETH,
+            server_url='http://0.0.0.0:80'
+        )
+    print(f" WETH balance: {res.amount}")
+    pass
+
+def print_eth_balance():
     res = compass.token.balance(
             chain=models.TokenBalanceChain.ETHEREUM_MAINNET,
             user=WALLET,
@@ -50,129 +58,292 @@ def print_eth_balance() -> float:
     print(f" ETH balance: {res.amount}")
     pass
 
-def print_vault_position(vault: str) -> dict:
-    res = compass.morpho.vault_position(
-        chain=models.MorphoVaultPositionChain.ETHEREUM_MAINNET,
-        user_address="0xa829B388A3DF7f581cE957a95edbe419dd146d1B",
-        vault_address=vault,
-        server_url='http://0.0.0.0:80'
-    )
-    # Handle response
-    print(f"vault contains: {res.token_amount} USDC")
-    pass
 
-
-def set_allowance_tx(contract: models.IncreaseAllowanceRequestContractName) -> dict:
-    res = compass.universal.allowance_set(
-        contract_name=contract,
-        amount=1000,
-        chain=models.Chain.ETHEREUM_MAINNET,
-        token=models.TokenEnum.USDC,
+def wrap_eth_tx() -> dict:
+    res = compass.universal.wrap_eth(
+        amount=1,
+        chain=chain,
         sender=WALLET,
-        #server_url="http://0.0.0.0:80",
+        server_url="http://0.0.0.0:80",
     )
     unsigned_transaction = res.model_dump(by_alias=True)
-    print(unsigned_transaction)
+    #print(unsigned_transaction)
+    print(w3.eth.send_transaction(unsigned_transaction).hex())
     return unsigned_transaction
 
-print('here')
 
+def uniswap_buy_tx() -> dict:
+    res = compass.uniswap_v3.swap_buy_exactly(
+        token_in=models.TokenEnum.WETH,
+        token_out=models.TokenEnum.USDC,
+        fee=models.FeeEnum.ZERO_DOT_01,
+        max_slippage_percent=0.5,
+        amount=1000,
+        wrap_eth=True,
+        chain=chain,
+        sender=WALLET,
+        server_url="http://0.0.0.0:80",
+    )
+    unsigned_transaction = res.model_dump(by_alias=True)
+    #print(unsigned_transaction)
+    print(w3.eth.send_transaction(unsigned_transaction).hex())
+    return unsigned_transaction
 
-
-print_balance()
 print_eth_balance()
+print_weth_balance()
+print_usdc_balance()
 
-print(set_allowance_tx(contract=models.IncreaseAllowanceRequestContractName.UNISWAP_V3_ROUTER))
-
-
-print('WITHDRAWING ALL USDC FROM MORPHO...')
-
-
-
-
-
+print('getting usdc + weth... ')
+wrap_eth_tx()
+time.sleep(4)
+uniswap_buy_tx()
+time.sleep(4)
 
 
-print_vault_position(usdc_vaults[0])
-print_vault_position(usdc_vaults[1])
-print_vault_position(usdc_vaults[2])
+print_eth_balance()
+print_weth_balance()
+print_usdc_balance()
 
-print(w3.eth.send_transaction(withdraw_tx(usdc_vaults[0])).hex())
-sleep(2)
-print(w3.eth.send_transaction(withdraw_tx(usdc_vaults[1])).hex())
-sleep(2)
-print(w3.eth.send_transaction(withdraw_tx(usdc_vaults[2])).hex())
-sleep(2)
+print_eth_balance()
+print('🏁 START 🏁')
 
 
-print_vault_position(usdc_vaults[0])
-print_vault_position(usdc_vaults[1])
-print_vault_position(usdc_vaults[2])
+def set_allowance_1():
+    res = compass.universal.allowance_set(
+        token=models.TokenEnum.USDC,
+        contract=models.SetAllowanceRequestContractEnum.AAVE_V3_POOL,
+        amount=100,
+        chain=chain,
+        sender=WALLET,
+        server_url="http://0.0.0.0:80",
+    )
+    unsigned_transaction = res.model_dump(by_alias=True)
+    gas_estimate = w3.eth.estimate_gas(unsigned_transaction)
+    w3.eth.send_transaction(unsigned_transaction).hex()
+    time.sleep(2)
+    print(f"⛽️ GAS ESTIMATE: {gas_estimate} ⛽️ -- set allowance: usdc aave")
 
 
-print('GET USDC BALANCE AGAIN:')
+def set_allowance_2():
+    res = compass.universal.allowance_set(
+        token=models.TokenEnum.WETH,
+        contract=models.SetAllowanceRequestContractEnum.UNISWAP_V3_ROUTER,
+        amount=1,
+        chain=chain ,
+        sender=WALLET,
+        server_url="http://0.0.0.0:80",
+    )
+    unsigned_transaction = res.model_dump(by_alias=True)
+    gas_estimate = w3.eth.estimate_gas(unsigned_transaction)
+    w3.eth.send_transaction(unsigned_transaction).hex()
+    time.sleep(2)
+    print(f"⛽️ GAS ESTIMATE: {gas_estimate} ⛽️ -- set allowance: weth uniswap")
 
-# wait for anvil to mine a block for the USDC balance to update
-sleep(2)
-print_balance()
-
-
-
-print('SET ALLOWANCE ON MORPHO:')
-
-
-# wait for anvil to mine a block for the USDC balance to update
-sleep(2)
-# 
-# res = compass.morpho.allowance(
-#     vault_address=usdc_vaults[0],
-#     amount=4,
-#     chain=models.MorphoSetVaultAllowanceRequestChain.ETHEREUM_MAINNET,
-#     sender=WALLET,
-#     server_url='http://0.0.0.0:80'
-# )
-# unsigned_transaction = res.model_dump(by_alias=True)
-# print(unsigned_transaction)
-
-
-
-
-
-print(w3.eth.send_transaction(set_allowance_tx(usdc_vaults[0])).hex())
-sleep(2)
-print(w3.eth.send_transaction(set_allowance_tx(usdc_vaults[1])).hex())
-sleep(2)
-print(w3.eth.send_transaction(set_allowance_tx(usdc_vaults[2])).hex())
-sleep(2)
+def aave_supply_1():
+    res = compass.aave_v3.supply(
+        token=models.TokenEnum.USDC,
+        amount=10,
+        chain=chain,
+        sender=WALLET,
+        server_url="http://0.0.0.0:80",
+    )
+    unsigned_transaction = res.model_dump(by_alias=True)
+    gas_estimate = w3.eth.estimate_gas(unsigned_transaction)
+    w3.eth.send_transaction(unsigned_transaction).hex()
+    time.sleep(2)
+    print(f"⛽️ GAS ESTIMATE: {gas_estimate} ⛽️ -- aave supply: USDC")
 
 
-#unsigned_transaction['nonce'] = 200
-# txn_hash = w3.eth.send_transaction(unsigned_transaction)
-# print(txn_hash.hex())
+def aave_borrow_1():
+    res = compass.aave_v3.borrow(
+        token=models.TokenEnum.WETH,
+        amount=0.00288642,
+        interest_rate_mode=models.InterestRateMode.VARIABLE,
+        chain=chain,
+        sender=WALLET,
+        server_url="http://0.0.0.0:80",
+    )
+    unsigned_transaction = res.model_dump(by_alias=True)
+    gas_estimate = w3.eth.estimate_gas(unsigned_transaction)
+    w3.eth.send_transaction(unsigned_transaction).hex()
+    time.sleep(2)
+    print(f"⛽️ GAS ESTIMATE: {gas_estimate} ⛽️ -- aave borrow: WETH")
+
+def uniswap_1():
+    res = compass.uniswap_v3.swap_sell_exactly(
+        token_in=models.TokenEnum.WETH,
+        token_out=models.TokenEnum.USDC,
+        fee=models.FeeEnum.ZERO_DOT_05,
+        amount=0.00288642,
+        max_slippage_percent=2.5,
+        chain=chain,
+        sender=WALLET,
+        wrap_eth=False,
+        server_url="http://0.0.0.0:80",
+    )
+    unsigned_transaction = res.model_dump(by_alias=True)
+    gas_estimate = w3.eth.estimate_gas(unsigned_transaction)
+    w3.eth.send_transaction(unsigned_transaction).hex()
+    time.sleep(2)
+    print(f"⛽️ GAS ESTIMATE: {gas_estimate} ⛽️ -- uniswap sell: WETH for USDC")
+
+
+def aave_supply_2():
+    res = compass.aave_v3.supply(
+        token=models.TokenEnum.USDC,
+        amount=6.983612,
+        chain=chain,
+        sender=WALLET,
+        server_url="http://0.0.0.0:80",
+    )
+    unsigned_transaction = res.model_dump(by_alias=True)
+    gas_estimate = w3.eth.estimate_gas(unsigned_transaction)
+    w3.eth.send_transaction(unsigned_transaction).hex()
+    time.sleep(2)
+    print(f"⛽️ GAS ESTIMATE: {gas_estimate} ⛽️ -- aave supply: USDC")
+
+
+def aave_borrow_2():
+    res = compass.aave_v3.borrow(
+        token=models.TokenEnum.WETH,
+        amount=Decimal(0.002015764334050235381693634693),
+        interest_rate_mode=models.InterestRateMode.VARIABLE,
+        chain=chain,
+        sender=WALLET,
+        server_url="http://0.0.0.0:80",
+    )
+    unsigned_transaction = res.model_dump(by_alias=True)
+    gas_estimate = w3.eth.estimate_gas(unsigned_transaction)
+    w3.eth.send_transaction(unsigned_transaction).hex()
+    time.sleep(2)
+    print(f"⛽️ GAS ESTIMATE: {gas_estimate} ⛽️ -- aave borrow: WETH")
+
+
+def uniswap_2():
+    res = compass.uniswap_v3.swap_sell_exactly(
+        token_in=models.TokenEnum.WETH,
+        token_out=models.TokenEnum.USDC,
+        fee=models.FeeEnum.ZERO_DOT_05,
+        amount=Decimal(0.002015764334050235381693634693),
+        max_slippage_percent=2.5,
+        chain=chain,
+        sender=WALLET,
+        wrap_eth=False,
+        server_url="http://0.0.0.0:80",
+    )
+    unsigned_transaction = res.model_dump(by_alias=True)
+    gas_estimate = w3.eth.estimate_gas(unsigned_transaction)
+    w3.eth.send_transaction(unsigned_transaction).hex()
+    time.sleep(2)
+    print(f"⛽️ GAS ESTIMATE: {gas_estimate} ⛽️ -- uniswap sell: WETH for USDC")
+
+def aave_supply_3():
+    res = compass.aave_v3.supply(
+        token=models.TokenEnum.USDC,
+        amount=4.877077016388,
+        chain=chain,
+        sender=WALLET,
+        server_url="http://0.0.0.0:80",
+    )
+    unsigned_transaction = res.model_dump(by_alias=True)
+    gas_estimate = w3.eth.estimate_gas(unsigned_transaction)
+    w3.eth.send_transaction(unsigned_transaction).hex()
+    time.sleep(2)
+    print(f"⛽️ GAS ESTIMATE: {gas_estimate} ⛽️ -- aave supply: USDC")
+
+
+def aave_borrow_3():
+    res = compass.aave_v3.borrow(
+        token=models.TokenEnum.WETH,
+        amount=0.0014077,
+        interest_rate_mode=models.InterestRateMode.VARIABLE,
+        chain=chain,
+        sender=WALLET,
+        server_url="http://0.0.0.0:80",
+    )
+    unsigned_transaction = res.model_dump(by_alias=True)
+    gas_estimate = w3.eth.estimate_gas(unsigned_transaction)
+    w3.eth.send_transaction(unsigned_transaction).hex()
+    time.sleep(2)
+    print(f"⛽️ GAS ESTIMATE: {gas_estimate} ⛽️ -- aave borrow: WETH")
+    
+    
+def uniswap_3():
+    res = compass.uniswap_v3.swap_sell_exactly(
+        token_in=models.TokenEnum.WETH,
+        token_out=models.TokenEnum.USDC,
+        fee=models.FeeEnum.ZERO_DOT_05,
+        amount=0.0014077,
+        max_slippage_percent=2.5,
+        chain=chain,
+        sender=WALLET,
+        wrap_eth=False,
+        server_url="http://0.0.0.0:80",
+    )
+    unsigned_transaction = res.model_dump(by_alias=True)
+    gas_estimate = w3.eth.estimate_gas(unsigned_transaction)
+    w3.eth.send_transaction(unsigned_transaction).hex()
+    time.sleep(2)
+    print(f"⛽️ GAS ESTIMATE: {gas_estimate} ⛽️ -- uniswap sell: WETH for USDC")
+
+
+def aave_supply_4():
+    res = compass.aave_v3.supply(
+        token=models.TokenEnum.USDC,
+        amount=3.1393109836119994,
+        chain=chain,
+        sender=WALLET,
+        server_url="http://0.0.0.0:80",
+    )
+    unsigned_transaction = res.model_dump(by_alias=True)
+    gas_estimate = w3.eth.estimate_gas(unsigned_transaction)
+    w3.eth.send_transaction(unsigned_transaction).hex()
+    time.sleep(2)
+    print(f"⛽️ GAS ESTIMATE: {gas_estimate} ⛽️ -- aave supply: USDC")
+    
+def aave_borrow_4():
+    res = compass.aave_v3.borrow(
+        token=models.TokenEnum.WETH,
+        amount=0.00090613727,
+        interest_rate_mode=models.InterestRateMode.VARIABLE,
+        chain=chain,
+        sender=WALLET,
+        server_url="http://0.0.0.0:80",
+    )
+    unsigned_transaction = res.model_dump(by_alias=True)
+    gas_estimate = w3.eth.estimate_gas(unsigned_transaction)
+    w3.eth.send_transaction(unsigned_transaction).hex()
+    time.sleep(2)
+    print(f"⛽️ GAS ESTIMATE: {gas_estimate} ⛽️ -- aave borrow: WETH")
+
+set_allowance_1()
+set_allowance_2()
+aave_supply_1()
+aave_borrow_1()
+uniswap_1()
+aave_supply_2()
+aave_borrow_2()
+uniswap_2()
+aave_supply_3()
+aave_borrow_3()
+uniswap_3()
+aave_supply_4()
+aave_borrow_4()
+
+
+# set_allowance_2(0.007)
+# set_allowance_2(1)
+# set_allowance_2(1000)
+# set_allowance_2(10000000)
 
 
 
+time.sleep(1)
+
+print_eth_balance()
+time.sleep(1)
 
 
-print('DEPOSIT ALL USDC INTO THIS VAULT:')
-
-# wait for anvil to mine a block for the USDC balance to update
-sleep(2)
-
-total = 8.53
-
-print(w3.eth.send_transaction(deposit_tx(vault=usdc_vaults[0], amount= total*0.33)).hex())
-sleep(2)
-print(w3.eth.send_transaction(deposit_tx(vault=usdc_vaults[1], amount=total*0.33)).hex())
-sleep(2)
-print(w3.eth.send_transaction(deposit_tx(vault=usdc_vaults[2], amount=total*0.33)).hex())
-sleep(2)
-
-
-
-print('GET BALANCE AGAIN')
-sleep(2)
-print_balance()
 
 
 
