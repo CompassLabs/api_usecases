e6570b87633ee3697a52cdec6077db665abb04dc
royalnine <17271567+royalnine@users.noreply.github.com>
2025-07-10 13:14:10 +0500

    change favicon

--- vault_rebalance/app/layout.tsx
+++ vault_rebalance/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from 'next';
 import './globals.css';
+
 export const metadata: Metadata = {
     title: 'Vault Rebalance',
     description: 'Make use of Compass Bundle to rebalance your vaults',
+    icons: {
+        icon: '/images/logo.svg',
+    },
 };
+
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en" data-oid="_051rw4">
