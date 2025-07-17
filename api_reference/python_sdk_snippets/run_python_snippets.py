1ff698178ed272897d8e7478ad26a044ff827dca
ccosnett <conor142857@icloud.com>
2025-07-17 13:22:10 +0100

    WIP

--- api_reference/python_sdk_snippets/run_python_snippets.py
+++ api_reference/python_sdk_snippets/run_python_snippets.py
@@ -36,62 +36,82 @@ def filter_paths(all_paths: List[str], broken: List[str]) -> List[str]:
 
 def main():
     API_URL = "https://spec.speakeasy.com/compasslabs/api/compass-api-with-code-samples"
-    BROKEN =  [
-    '/v0/aave/supply',
-    '/v0/aave/borrow',
-    '/v0/aave/repay',
-    '/v0/aave/withdraw',
-    '/v0/aerodrome_slipstream/swap/sell_exactly',
-    '/v0/aerodrome_slipstream/swap/buy_exactly',
-    '/v0/aerodrome_slipstream/liquidity_provision/mint',
-    '/v0/aerodrome_slipstream/liquidity_provision/increase',
-    '/v0/aerodrome_slipstream/liquidity_provision/withdraw',
-    '/v0/morpho/markets',
-    '/v0/morpho/allowance',
-    '/v0/morpho/deposit',
-    '/v0/morpho/withdraw',
-    '/v0/morpho/supply_collateral',
-    '/v0/morpho/withdraw_collateral',
-    '/v0/morpho/borrow',
-    '/v0/morpho/repay',
-    '/v0/sky/buy',
-    '/v0/sky/sell',
-    '/v0/sky/deposit',
-    '/v0/sky/withdraw',
-    '/v0/token/balance/get',
-    '/v0/token/transfer',
-    '/v0/uniswap/quote/buy_exactly/get',
-    '/v0/uniswap/quote/sell_exactly/get',
-    '/v0/uniswap/swap/buy_exactly',
-    '/v0/uniswap/swap/sell_exactly',
-    '/v0/uniswap/liquidity_provision/increase',
-    '/v0/uniswap/liquidity_provision/mint',
-    '/v0/uniswap/liquidity_provision/withdraw',
-    '/v0/generic/portfolio/get',
-    '/v0/generic/visualize_portfolio/get',
-    '/v0/generic/wrap_eth',
-    '/v0/generic/unwrap_weth',
-    '/v0/generic/allowance/set',
-    '/v0/pendle/buy_pt',
-    '/v0/pendle/sell_pt',
-    '/v0/pendle/buy_yt',
-    '/v0/pendle/sell_yt',
-    '/v0/pendle/redeem_yield',
-    '/v0/pendle/add_liquidity',
-    '/v0/pendle/remove_liquidity'
+    BROKEN = [
+        "/v0/aave/supply",
+        "/v0/aave/borrow",
+        "/v0/aave/repay",
+        "/v0/aave/withdraw",
+        "/v0/aave/historical_transactions/get",
+        "/v0/aerodrome_slipstream/swap/sell_exactly",
+        "/v0/aerodrome_slipstream/swap/buy_exactly",
+        "/v0/aerodrome_slipstream/liquidity_provision/mint",
+        # "/v0/aerodrome_slipstream/liquidity_provision/increase",
+        # "/v0/aerodrome_slipstream/liquidity_provision/withdraw",
+        # "/v0/morpho/markets",
+        # "/v0/morpho/allowance",
+        # "/v0/morpho/deposit",
+        # "/v0/morpho/withdraw",
+        # "/v0/morpho/supply_collateral",
+        # "/v0/morpho/withdraw_collateral",
+        # "/v0/morpho/borrow",
+        # "/v0/morpho/repay",
+        # "/v0/morpho/vault",
+        # "/v0/morpho/vaults",
+        # "/v0/morpho/user_position",
+        # "/v0/sky/buy",
+        # "/v0/sky/sell",
+        # "/v0/sky/deposit",
+        # "/v0/sky/withdraw",
+        # "/v0/token/balance/get",
+        # "/v0/token/transfer",
+        # "/v0/token/price/get",
+        # "/v0/uniswap/quote/buy_exactly/get",
+        # "/v0/uniswap/quote/sell_exactly/get",
+        # "/v0/uniswap/swap/buy_exactly",
+        # "/v0/uniswap/swap/sell_exactly",
+        # "/v0/uniswap/liquidity_provision/increase",
+        # "/v0/uniswap/liquidity_provision/mint",
+        # "/v0/uniswap/liquidity_provision/withdraw",
+        # "/v0/generic/portfolio/get",
+        # "/v0/generic/visualize_portfolio/get",
+        # "/v0/generic/wrap_eth",
+        # "/v0/generic/unwrap_weth",
+        # "/v0/generic/allowance/set",
+        # "/v0/pendle/buy_pt",
+        # "/v0/pendle/sell_pt",
+        # "/v0/pendle/buy_yt",
+        # "/v0/pendle/sell_yt",
+        # "/v0/pendle/redeem_yield",
+        # "/v0/pendle/add_liquidity",
+        # "/v0/pendle/remove_liquidity",
     ]
 
     api_key = load_api_key()
     spec = fetch_api_spec(API_URL)
 
     all_paths = list(spec.get("paths", {}))
-    good_paths = filter_paths(all_paths, BROKEN)
+    new_broken = []
 
-    for path in good_paths:
-        print(f"----------------\n"
-              f"endpoint: {path}")
-        code = get_python_code(path, spec)
-        exec(replace_with_secret(code, api_key))
+    for path in all_paths:
+        print(f"----------------\nendpoint: {path}")
+        try:
+            code = get_python_code(path, spec)
+            exec(replace_with_secret(code, api_key))
+        except Exception as e:
+            print(f"❌ Failed: {path} – {e}")
+            new_broken.append(path)
+
+    print("\nFailed paths:")
+    for path in new_broken:
+        print(f"- {path}")
+    #good_paths = filter_paths(all_paths, BROKEN)
+
+    # for path in good_paths:
+    #     print(f"----------------\n"
+    #           f"endpoint: {path}")
+    #     code = get_python_code(path, spec)
+    #     exec(replace_with_secret(code, api_key))
 
 if __name__ == "__main__":
+    
     main()
