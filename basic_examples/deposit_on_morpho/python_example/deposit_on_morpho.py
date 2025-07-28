48c643d7974fe3d36a96ad0e167c3d7bdde26a5f
ccosnett <conor142857@icloud.com>
2025-07-28 16:26:27 +0100

    WIP

--- basic_examples/deposit_on_morpho/python_example/deposit_on_morpho.py
+++ basic_examples/deposit_on_morpho/python_example/deposit_on_morpho.py
@@ -1,8 +1,8 @@
-
 from compass_api_sdk import CompassAPI, models
 from dotenv import load_dotenv
 import os
 from web3 import Web3, HTTPProvider
+
 load_dotenv()
 COMPASS_API_KEY = os.getenv("COMPASS_API_KEY")
 PRIVATE_KEY = os.getenv("PRIVATE_KEY")
@@ -11,6 +11,7 @@ SPECIFIC_MORPHO_VAULT = os.getenv("SPECIFIC_MORPHO_VAULT")
 BASE_RPC_URL = os.getenv("BASE_RPC_URL")
 w3 = Web3(HTTPProvider(BASE_RPC_URL))
 compass = CompassAPI(api_key_auth=COMPASS_API_KEY)
+
 
 # Helper function:
 def send_tx(response):
@@ -22,36 +23,28 @@ def send_tx(response):
     return tx_hash, dict(receipt)
 
 
-
 # SET ALLOWANCE
+# Get unsigned Allowance Transaction from the Compass API
+response = compass.universal.allowance_set(
+    token=models.TokenEnum.USDC,
+    contract=SPECIFIC_MORPHO_VAULT,
+    amount=1,
+    chain=models.Chain.BASE_MAINNET,
+    sender=WALLET_ADDRESS,
+)
 
-response = compass.universal.allowance_set(token=models.TokenEnum.USDC, contract=SPECIFIC_MORPHO_VAULT, amount=1, chain=models.Chain.BASE_MAINNET, sender=WALLET_ADDRESS)
-
-
-
-# SEND ALLOWANCE TRANSACTION
-
-
-
+# Sign and broadcast allowance transaction
 print(send_tx(response))
 
-
-
 # DEPOSIT ON MORPHO
-
-
-
-res = compass.morpho.deposit(vault_address=SPECIFIC_MORPHO_VAULT, amount=1, chain=models.MorphoDepositRequestChain.BASE_MAINNET, sender=WALLET_ADDRESS)
+# Get unsigned transaction
+res = compass.morpho.deposit(
+    vault_address=SPECIFIC_MORPHO_VAULT,
+    amount=1,
+    chain=models.MorphoDepositRequestChain.BASE_MAINNET,
+    sender=WALLET_ADDRESS,
+)
 print(res.model_dump())
 
-#    Handle response
-print(res)
-
-
-# SEND DEPOSIT TRANSACTION
-
-
-
+# Sign and broadcast transaction
 print(send_tx(res))
-
-
