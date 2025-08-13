281080e5d84948a7ae7072dc1bfa54be1e6908fe
Lukas Scheucher <scheuclu@gmail.com>
2025-08-13 10:43:15 +0200

    WIP

--- v0/basic_examples/deposit_on_aave/python_example/main.py
+++ v0/basic_examples/deposit_on_aave/python_example/main.py
@@ -7,22 +7,25 @@ from web3 import Web3, HTTPProvider
 
 load_dotenv()
 COMPASS_API_KEY = os.getenv("COMPASS_API_KEY")
+assert COMPASS_API_KEY, "Compass API requires an API key. You can sign up for free"
 PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+assert PRIVATE_KEY
 WALLET_ADDRESS = os.getenv("WALLET_ADDRESS")
-SPECIFIC_MORPHO_VAULT = os.getenv("SPECIFIC_MORPHO_VAULT")
-BASE_RPC_URL = os.getenv("BASE_RPC_URL")
+assert WALLET_ADDRESS
+SPECIFIC_MORPHO_VAULT = "0x616a4E1db48e22028f6bbf20444Cd3b8e3273738" #Seamless USDC vault
+BASE_RPC_URL = os.getenv("BASE_RPC_URL","https://base-rpc.publicnode.com")
+
+
 w3 = Web3(HTTPProvider(BASE_RPC_URL))
 compass = CompassAPI(api_key_auth=COMPASS_API_KEY)
 
 # SNIPPET END 1
 
-
-
 # SNIPPET START 2
 # Helper function to sign and broadcast unsigned transaction:
 def send_tx(response):
     tx = response.model_dump(by_alias=True)
-    signed_tx = w3.eth.account.sign_transaction(tx, PRIVATE_KEY)
+    signed_tx = w3.eth.account.sign_transaction(tx['transaction'], PRIVATE_KEY)
     tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction).hex()
     receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
     # convert receipt to a serializable dict
@@ -33,11 +36,11 @@ def send_tx(response):
 # SNIPPET START 3
 # SET ALLOWANCE
 # Get unsigned Allowance Transaction from the Compass API
-response = compass.universal.allowance_set(
+response = compass.universal.generic_allowance_set(
     token=models.TokenEnum.USDC,
     contract=SPECIFIC_MORPHO_VAULT,
-    amount=1,
-    chain=models.Chain.BASE_MAINNET,
+    amount=0.001,
+    chain=models.SetAllowanceRequestChain.BASE,
     sender=WALLET_ADDRESS,
 )
 # SNIPPET END 3
@@ -52,10 +55,10 @@ print(send_tx(response))
 # SNIPPET START 5
 # DEPOSIT ON MORPHO
 # Get unsigned transaction
-res = compass.morpho.deposit(
+res = compass.morpho.morpho_deposit(
     vault_address=SPECIFIC_MORPHO_VAULT,
-    amount=1,
-    chain=models.MorphoDepositRequestChain.BASE_MAINNET,
+    amount=0.001,
+    chain=models.MorphoDepositRequestChain.BASE,
     sender=WALLET_ADDRESS,
 )
 print(res.model_dump())
