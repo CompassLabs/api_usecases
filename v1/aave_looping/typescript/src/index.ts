6ef3a6f26f1f0d18a84204fb24c52621cc34d849
stefan-joseph <stefanjoseph.dev@gmail.com>
2025-08-14 15:22:27 -0500

    wip

--- v1/aave_looping/typescript/src/index.ts
+++ v1/aave_looping/typescript/src/index.ts
@@ -1,30 +1,30 @@
 // SNIPPET START 1
 import { CompassApiSDK } from "@compass-labs/api-sdk";
+import dotenv from "dotenv";
 import { privateKeyToAccount } from "viem/accounts";
 import { mainnet } from "viem/chains";
 import { createPublicClient, http } from "viem";
 import { createWalletClient } from "viem";
-import dotenv from "dotenv";
-import { SendTransactionRequest } from "viem";
+import { type BundlerTransactionResponse } from "@compass-labs/api-sdk/models/components";
 
 dotenv.config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY as `0x${string}`;
 const ETHEREUM_RPC_URL = process.env.ETHEREUM_RPC_URL as string;
+// SNIPPET END 1
 
+// SNIPPET START 2
 const compassApiSDK = new CompassApiSDK({
   apiKeyAuth: process.env.COMPASS_API_KEY,
   serverURL: process.env.SERVER_URL || undefined, // For internal testing purposes. You do not need to set this.
 });
-// SNIPPET END 1
 
-// SNIPPET START 2
 const account = privateKeyToAccount(PRIVATE_KEY);
 
 const walletClient = createWalletClient({
   account,
   chain: mainnet,
-  transport: http(ETHEREUM_RPC_URL),
+  transport: http(ETHEREUM_RPC_URL as string),
 });
 
 const publicClient = createPublicClient({
@@ -47,27 +47,9 @@ const signedAuth = await walletClient.signAuthorization({
 });
 // SNIPPET END 3
 
-const swapTX = await compassApiSDK.swap.swapOdos({
-  chain: "ethereum",
-  sender: account.address,
-  tokenIn: "ETH",
-  tokenOut: "WETH",
-  amount: 1,
-  maxSlippagePercent: 1,
-});
-
-const swapTxHash = await walletClient.sendTransaction({
-  ...(swapTX.transaction as any),
-  value: BigInt(swapTX.transaction.value), // Convert to BigInt
-});
-
-await publicClient.waitForTransactionReceipt({
-  hash: swapTxHash,
-});
-
 // SNIPPET START 4
-const bundlerTx =
-  await compassApiSDK.transactionBundler.transactionBundlerExecute({
+const loopingTx =
+  (await compassApiSDK.transactionBundler.transactionBundlerAaveLoop({
     chain: "ethereum",
     sender: account.address,
     signedAuthorization: {
@@ -78,31 +60,22 @@ const bundlerTx =
       s: signedAuth.s,
       yParity: signedAuth.yParity as number,
     },
-    actions: [
-      {
-        body: {
-          actionType: "SET_ALLOWANCE",
-          token: "WETH",
-          contract: "UniswapV3Router",
-          amount: 1,
-        },
-      },
-      {
-        body: {
-          actionType: "UNISWAP_SELL_EXACTLY",
-          amountIn: 1,
-          fee: "0.01",
-          maxSlippagePercent: 0.5,
-          tokenIn: "WETH",
-          tokenOut: "USDC",
-        },
-      },
-    ],
-  });
+    collateralToken: "USDC",
+    borrowToken: "WETH",
+    initialCollateralAmount: 4,
+    multiplier: 1.5,
+    maxSlippagePercent: 2.5,
+    loanToValue: 70,
+  })) as BundlerTransactionResponse;
+
 // SNIPPET END 4
 
 // SNIPPET START 5
-const txHash = await walletClient.sendTransaction(bundlerTx.transaction as any);
+const bunderTx = await walletClient.sendTransaction(
+  loopingTx.transaction as any
+);
+
+const txHash = await walletClient.sendTransaction(bunderTx as any);
 
 const receipt = await publicClient.waitForTransactionReceipt({
   hash: txHash,
