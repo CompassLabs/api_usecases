eb654bab822f8b0b930774d02feab803df7fbb71
ccosnett <conor142857@icloud.com>
2025-08-23 13:00:17 +0100

    WIP

--- v1/basic_examples/deposit_on_aave/python_example/main.py
+++ v1/basic_examples/deposit_on_aave/python_example/main.py
@@ -1,76 +1,95 @@
 # SNIPPET START 1
+import time
 
 from compass_api_sdk import CompassAPI, models
-from dotenv import load_dotenv
 import os
-from web3 import Web3, HTTPProvider
-from web3 import Account
-
+import dotenv
+from web3 import Web3
+from eth_account import Account
+from dotenv import load_dotenv
+import devtools
 
 load_dotenv()
-COMPASS_API_KEY = os.getenv("COMPASS_API_KEY")
-assert COMPASS_API_KEY, "Compass API requires an API key. You can sign up for free"
+
 PRIVATE_KEY = os.getenv("PRIVATE_KEY")
-#assert PRIVATE_KEY
-SPECIFIC_MORPHO_VAULT = "0x616a4E1db48e22028f6bbf20444Cd3b8e3273738" #Seamless USDC vault
-#os.getenv("BASE_RPC_URL","https://base-rpc.publicnode.com")
+assert PRIVATE_KEY
 BASE_RPC_URL = os.getenv("BASE_RPC_URL")
-SERVER_URL = os.getenv("SERVER_URL")
-assert SERVER_URL
-#WALLET_ADDRESS = Account.from_key(PRIVATE_KEY).address
-WALLET_ADDRESS = os.getenv("WALLET_ADDRESS")
-assert WALLET_ADDRESS
+COMPASS_API_KEY = os.getenv("COMPASS_API_KEY")
+assert COMPASS_API_KEY
+CHAIN = models.Chain.BASE
+ETH = models.TokenEnum.ETH
+USDC = models.TokenEnum.USDC
+SPECIFIC_MORPHO_VAULT = os.getenv("SPECIFIC_MORPHO_VAULT")
 
+w3 = Web3(Web3.HTTPProvider(BASE_RPC_URL))
 
-w3 = Web3(HTTPProvider(BASE_RPC_URL))
-compass = CompassAPI(api_key_auth=COMPASS_API_KEY, server_url=SERVER_URL) # TODO(ccosnett) make this optional... etc.. like in typescript
+compass = CompassAPI(
+    api_key_auth=COMPASS_API_KEY,
+    server_url=os.getenv("SERVER_URL")
+    or None,  # For internal testing purposes. You do not need to set this.
+)
 
+account = Account.from_key(PRIVATE_KEY)
+WALLET_ADDRESS = account.address
 # SNIPPET END 1
+
 
 # SNIPPET START 2
 # Helper function to sign and broadcast unsigned transaction:
 def send_tx(response):
     tx = response.model_dump(by_alias=True)
-    signed_tx = w3.eth.account.sign_transaction(tx['transaction'], PRIVATE_KEY)
+    signed_tx = w3.eth.account.sign_transaction(tx["transaction"], PRIVATE_KEY)
     tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction).hex()
+    start = time.time()
     receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    end = time.time()
+    print(f"⏱️ Time waiting for receipt: {end - start:.2f} seconds")
     # convert receipt to a serializable dict
-    return tx_hash, dict(receipt)
+    return tx_hash  # , dict(receipt) # <- uncomment if you need to see the tx receipt
+
+
 # SNIPPET END 2
 
 
-# SNIPPET START 3
-# SET ALLOWANCE
-# Get unsigned Allowance Transaction from the Compass API
-response = compass.universal.generic_allowance_set(
-    token=models.TokenEnum.USDC,
-    contract=SPECIFIC_MORPHO_VAULT,
-    amount=0.001,
-    chain=models.SetAllowanceRequestChain.BASE,
+# assuming your wallet has ETH but not USDC. We sell 0.03 USD of ETH for USDC.
+
+one_USD_in_ETH = 1 / float(compass.token.token_price(chain=CHAIN, token=ETH).price)
+
+# using ODOS to perform the swap
+swap_tx = compass.swap.swap_odos(
+    chain=CHAIN,
     sender=WALLET_ADDRESS,
+    token_in=ETH,
+    token_out=USDC,
+    amount=0.03 * one_USD_in_ETH,  # amount in = 0.03 USD in ETH
+    max_slippage_percent=1,
 )
-# SNIPPET END 3
 
+devtools.debug(send_tx(swap_tx))
 
-# SNIPPET START 4
-# Sign and broadcast allowance transaction
-print(send_tx(response))
-# SNIPPET END 4
+time.sleep(1)
 
-
-# SNIPPET START 5
-# DEPOSIT ON MORPHO
-# Get unsigned transaction
-res = compass.morpho.morpho_deposit(
-    vault_address=SPECIFIC_MORPHO_VAULT,
-    amount=0.001,
-    chain=models.MorphoDepositRequestChain.BASE,
+allowance_tx = compass.universal.generic_allowance_set(
+    chain=CHAIN,
     sender=WALLET_ADDRESS,
+    contract=models.SetAllowanceRequestContractEnum.AAVE_V3_POOL, 
+    amount=0.01,
+    token=USDC,
 )
-print(res.model_dump())
 
-# SNIPPET START 6
-# Sign and broadcast deposit transaction
-print(send_tx(res))
-# SNIPPET END 6
+devtools.debug(send_tx(allowance_tx))
 
+devtools.debug(
+    compass.universal.generic_allowance(
+        chain=CHAIN, token=USDC, user=WALLET_ADDRESS, contract=models.SetAllowanceRequestContractEnum.AAVE_V3_POOL
+    ).amount
+)
+
+
+deposit_tx = compass.aave_v3.aave_supply(
+    chain=CHAIN,
+    sender=WALLET_ADDRESS,
+    token=USDC,  # seamless USDC Vault.
+    amount=0.01,
+)
+devtools.debug(send_tx(deposit_tx))
